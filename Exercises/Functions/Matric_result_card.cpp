764ab829692a9e0928ca64dbd456f1259aa6c931
Muhammad Haseeb <mhasop997@gmail.com>
2025-07-29 12:41:57 +0500

    Update Matric_result_card.cpp

--- Exercises/Functions/Matric_result_card.cpp
+++ Exercises/Functions/Matric_result_card.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cctype>
 
 using namespace std;
 
@@ -20,7 +21,7 @@ int Sumof9thmarks(int arr[8])
     {
         sum += arr[i];
     }
-    return sum;   
+    return sum;
 }
 
 int Sumof10thmarks(int arr[8])
@@ -46,28 +47,51 @@ int Sumof9th10thmarks(int arr[8])
 float Perof9thmarks(int a)
 {
     float per = (a / 550.0) * 100;
-    return per;    
+    return per;
 }
 
 float Perof10thmarks(int a)
 {
     float per = (a / 550.0) * 100;
-    return per;        
+    return per;
 }
 
 float Perof9th10thmarks(int a)
 {
     float per = (a / 1100.0) * 100;
-    return per;        
+    return per;
 }
 
 int main()
 {
     system("cls");
 
-    cout << "***Program that give you complete analysis of your 9th and 10th class (of ICS group)***\n";
+    cout << "***Program that give you complete analysis of your 9th and 10th class***\n\n";
 
-    string sub[8] = {"English", "Urdu", "Maths", "Physics", "Chemistry", "Pakistan Studies", "Islamiat", "Computer"};
+    string group;
+    cout << "Enter the group Biology or Computer: ";
+    cin >> group;
+
+    for (char &ch : group)
+    {
+        ch = tolower(ch); // convert each character to lowercase
+    }
+
+    cout << endl;
+
+    string sub[8] = {"English", "Urdu", "Maths", "Physics", "Chemistry", "Pakistan Studies", "Islamiat"};
+
+    if (group == "biology" || group == "computer")
+    {
+        sub[7] = (group == "biology") ? "Biology" : "Computer";
+        cout << endl;
+    }
+
+    else
+    {
+        cout << "Invalid Group" << endl;
+    }
+
     int ninth_marks[8], tenth_marks[8], tot_mar_per_sub[8], diff_mar_per_sub[8];
 
     for (int i = 0; i < 8; i++)
@@ -91,8 +115,9 @@ int main()
     }
 
     cout << left << setw(20) << "Subjects" << setw(17) << "9th Marks" << setw(17) << "10th Marks" << setw(17) << "Obtained Marks" << setw(17) << "Analysis" << endl;
-    cout << "____________________________________________________________________________________________________________________" << endl << endl;
-    
+    cout << "____________________________________________________________________________________________________________________" << endl
+         << endl;
+
     for (int i = 0; i < 8; i++)
     {
         cout << left << setw(20) << sub[i] << setw(17) << ninth_marks[i] << setw(17) << tenth_marks[i] << setw(17) << tot_mar_per_sub[i];
@@ -109,29 +134,34 @@ int main()
             cout << "Decreased by " << diff_mar_per_sub[i] * -1 << "marks less than that in 9th class" << endl;
         }
     }
-    cout << "____________________________________________________________________________________________________________________" << endl << endl;
+    cout << "____________________________________________________________________________________________________________________" << endl
+         << endl;
 
-    cout << left << setw(20) << "Total Marks" << setw(17) << "550" << setw(17) << "550" << setw(17) << "1100" <<endl; 
+    cout << left << setw(20) << "Total Marks" << setw(17) << "550" << setw(17) << "550" << setw(17) << "1100" << endl;
 
     int sum9mar = Sumof9thmarks(ninth_marks), sum10mar = Sumof10thmarks(tenth_marks), sum910mar = Sumof9th10thmarks(tot_mar_per_sub);
 
-    cout << left << setw(20) << "Obtained Marks" << setw(17) << sum9mar << setw(17) << sum10mar << setw(17) << sum910mar <<endl;
+    cout << left << setw(20) << "Obtained Marks" << setw(17) << sum9mar << setw(17) << sum10mar << setw(17) << sum910mar << endl;
 
-    cout << left << setw(20) << "Percentage" << setw(17) << Perof9thmarks(float(sum9mar)) << setw(17) << Perof10thmarks(float(sum10mar)) << setw(17) << Perof9th10thmarks(float(sum910mar)) << endl << endl;
-    
+    cout << left << setw(20) << "Percentage" << setw(17) << Perof9thmarks(float(sum9mar)) << setw(17) << Perof10thmarks(float(sum10mar)) << setw(17) << Perof9th10thmarks(float(sum910mar)) << endl
+         << endl;
+
     if (Perof10thmarks(float(sum10mar)) < Perof9thmarks(float(sum9mar)))
     {
-        cout << "Decreased by " << (Perof9thmarks(float(sum9mar)) - Perof10thmarks(float(sum10mar))) << "% marks" << endl << endl;
+        cout << "Decreased by " << (Perof9thmarks(float(sum9mar)) - Perof10thmarks(float(sum10mar))) << "% marks" << endl
+             << endl;
     }
 
     else if (Perof10thmarks(float(sum10mar)) > Perof9thmarks(float(sum9mar)))
     {
-        cout << "Improved by " << (Perof10thmarks(float(sum10mar)) - Perof9thmarks(float(sum9mar))) << "% marks" << endl << endl;    
+        cout << "Improved by " << (Perof10thmarks(float(sum10mar)) - Perof9thmarks(float(sum9mar))) << "% marks" << endl
+             << endl;
     }
     else
     {
-        cout << "No Change" << endl << endl;
-    }      
+        cout << "No Change" << endl
+             << endl;
+    }
 
     system("pause");
     return 0;
