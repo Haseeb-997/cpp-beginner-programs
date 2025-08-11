d5f9a052ca13334f5f49d14f05a2697b218d4838
Muhammad Haseeb <mhasop997@gmail.com>
2025-08-11 10:42:01 +0500

    Update Weekday_Weakend.cpp

--- Exercises/If else/Weekday_Weakend.cpp
+++ Exercises/If else/Weekday_Weakend.cpp
@@ -6,30 +6,27 @@ int main()
     std::cout << "***Program that tells whether a day is a weekday or weakend***\n";
     std::cout << "1:\"Monday\"\n2:\"Tuesday\"\n3:\"Wednesday\"\n4:\"Thursday\"\n5:\"Friday\"\n6:\"Saturday\"\n7:\"Sunday\"\n";
     int day;
-    bool day_num;
     do
     {
-        day_num = 1;
         std::cout << "Enter the number (corresponding day) between 1 and 7: ";
         std::cin >> day;
         if ((day >= 1) && (day <= 7))
         {
             if ((day == 6) || (day == 7))
             {
-                std::cout << "\n\"Weekend\"\n\n";
+                std::cout << "\n\"Weekend\"\n\n";             
             }
             else
             {
                 std::cout << "\n\"Weekday\"\n\n";
             }
+            break;                    
         }
         else
         {
             std::cout << "\nInvalid Day Number\n\n";
-            day_num = 0;
         }
-    } while (day_num == 0);
+    } while (true);
     system("pause");
     return 0;
-
 }
