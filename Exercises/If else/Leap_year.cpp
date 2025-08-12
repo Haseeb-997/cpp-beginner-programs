671ae3888ddc14cc3739352fde0a66658760a513
Muhammad Haseeb <mhasop997@gmail.com>
2025-08-12 21:50:56 +0500

    Add files via upload

---
+++ Exercises/If else/Leap_year.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main()
+{
+    int year;
+    std::cout << "\n***The program that will tell you whether the year is \"leap\" or not***\n\n";
+    while (true)
+    {
+        std::cout << "Enter the year: ";
+        std::cin >> year;
+        if (year > 0)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 != 0)
+                {
+                    std::cout << "\n" << year << " is a Leap Year\n\n";
+                }
+                else if ((year % 100 == 0) && (year % 400 == 0))
+                {
+                    std::cout << "\n" << year << " is a Leap Year\n\n";
+                }
+                else
+                {
+                    std::cout << "\n" << year << " is not a Leap Year\n\n";
+                }
+                break;
+            }
+            else
+            {
+                std::cout << "\n" << year << " is not a Leap Year\n\n";
+            }
+            break;
+        }
+        else
+        {
+            std::cout << "\nInvalid Year\nEnter a valid year!\n\n";
+        }
+    }
+    return 0;
+}
