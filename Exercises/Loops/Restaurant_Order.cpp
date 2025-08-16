2ac70b0ca6723b20b5c1d72d4fd8009954acdd52
Muhammad Haseeb <mhasop997@gmail.com>
2025-08-16 13:12:51 +0500

    Update Restaurant_Order.cpp

--- Exercises/Loops/Restaurant_Order.cpp
+++ Exercises/Loops/Restaurant_Order.cpp
@@ -1,5 +1,78 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void breakfast_choice(int bch)
+{
+    switch (bch)
+    {
+    case 1:
+        system("pause");
+        break;
+    case 2:
+        system("pause");
+        break;
+    case 3:
+        break;
+    default:
+        cout << "\nInvalid Input!\nEnter valid one: \n\n";
+        system("pause");
+    }
+}
+
+void lunch_choice(int lch)
+{
+    switch (lch)
+    {
+    case 1:
+        system("pause");
+        break;
+    case 2:
+        system("pause");
+        break;
+    case 3:
+        break;
+    default:
+        cout << "\nInvalid Input!\nEnter valid one\n\n";
+        system("pause");
+    }
+}
+
+void fastfood_choice(int fch)
+{
+    switch (fch)
+    {
+    case 1:
+        system("pause");
+        break;
+    case 2:
+        system("pause");
+        break;
+    case 3:
+        break;
+    default:
+        cout << "\nInvalid Input!\nEnter valid one\n\n";
+        system("pause");
+    }
+}
+
+void dinner_choice(int dch)
+{
+    switch (dch)
+    {
+    case 1:
+        system("pause");
+        break;
+    case 2:
+        system("pause");
+        break;
+    case 3:
+        break;
+    default:
+        cout << "\nInvalid Input!\nEnter valid one\n\n";
+        system("pause");
+    }
+}
 
 int main()
 {
@@ -18,144 +91,56 @@ int main()
             {
                 system("cls");
                 cout << "********************BREAKFAST********************\n\n";
-                cout << "1. PURI\n2. HALWA\n3. LACHCHA PARATHA\n4. OMELETTE\n5. HALF FRY EGG\n6. TEA\n7. LASSI\n8. DAHI\n9. WATER\n10. BACK TO MAIN MENU\n\n";
-                cout << "Enter your choice(1-10): ";
+                cout << left << setw(30) << "\"ITEMS\"" << setw(30) << "\"PRICES\"" << "\n";
+                cout << setw(30) << "1. PURI" << " Rs.10/-" << "\n"
+                     << setw(30) << "2. HALWA" << " Rs.90/Pao" << "\n"
+                     << "3. BACK TO MAIN MENU" << "\n\n";
+                cout << "Enter your choice(1-3): ";
                 cin >> BCH;
-                switch (BCH)
-                {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                default:
-                    cout << "\nInvalid Input!\nEnter valid one\n\n";
-                    system("pause");
-                }
-            } while (BCH != 10);
+                breakfast_choice(BCH);
+            } while (BCH != 3);
             break;
         case 2:
             do
             {
                 system("cls");
                 cout << "********************LUNCH********************\n\n";
-                cout << "1. CHICKEN KORMA\n2. VEGETABLE\n3. MIX DAAL\n4. CURRY PAKORA\n5. ALU KOFTA\n6. ALU KEEMA\n7. NAAN/ROTI\n8. COLD DRINKS\n9. SALAD\n10. BACK TO MAIN MENU\n\n";
-                cout << "Enter your choice(1-10): ";
+                cout << left << setw(30) << "\"ITEMS\"" << setw(30) << "\"PRICES\"" << "\n";
+                cout << setw(30) << "1. CHICKEN KORMA" << " Rs.550/-" << "\n"
+                     << setw(30) << "2. VEGETABLE" << " Rs.250/-" << "\n"
+                     << "3. BACK TO MAIN MENU" << "\n\n";
+                cout << "Enter your choice(1-3): ";
                 cin >> LCH;
-                switch (LCH)
-                {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                default:
-                    cout << "\nInvalid Input!\nEnter valid one\n\n";
-                    system("pause");
-                }
-            } while (LCH != 10);
+                lunch_choice(LCH);
+            } while (LCH != 3);
             break;
         case 3:
             do
             {
                 system("cls");
                 cout << "********************FAST FOOD********************\n\n";
-                cout << "1. SAHWARMA\n2. BURGER\n3. WRAPS\n4. PIZZA\n5. FRIES\n6. CHICKEN WINGS\n7. AFGHANI FRIES\n8. SAMOSA CHAAT\n9. SODA\n10. BACK TO MAIN MENU\n\n";
-                cout << "Enter your choice(1-10): ";
+                cout << left << setw(30) << "\"ITEMS\"" << setw(30) << "\"PRICES\"" << "\n";
+                cout << setw(30) << "1. SAHWARMA" << " Rs.200/-" << "\n"
+                     << setw(30) << "2. BURGER" << " Rs.200/-" << "\n"
+                     << "3. BACK TO MAIN MENU" << "\n\n";
+                cout << "Enter your choice(1-3): ";
                 cin >> FCH;
-                switch (FCH)
-                {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                default:
-                    cout << "\nInvalid Input!\nEnter valid one\n\n";
-                    system("pause");
-                }
-            } while (FCH != 10);
+                fastfood_choice(FCH);
+            } while (FCH != 3);
             break;
         case 4:
             do
             {
                 system("cls");
                 cout << "********************DINNER********************\n\n";
-                cout << "1. DAAL CHAWAL\n2. NEHARI\n3. DHALEEM\n4. MALAI BOTI\n5. SEAKH KABAB\n6. BEAF KARAHI\n7. RAITA\n8. NAAN/ROTI\n9. SALAD\n10. BACK TO MAIN MENU\n\n";
-                cout << "Enter your choice(1-10): ";
+                cout << left << setw(30) << "\"ITEMS\"" << setw(30) << "\"PRICES\"" << "\n";
+                cout << setw(30) << "1. DAAL CHAWAL" << " Rs.250/-" << "\n"
+                     << setw(30) << "2. NEHARI" << " Rs.450/-" << "\n"
+                     << "3. BACK TO MAIN MENU" << "\n\n";
+                cout << "Enter your choice(1-3): ";
                 cin >> DCH;
-                switch (DCH)
-                {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                default:
-                    cout << "\nInvalid Input!\nEnter valid one\n\n";
-                    system("pause");
-                }
-            } while (DCH != 10);
+                dinner_choice(DCH);
+            } while (DCH != 3);
             break;
         case 5:
             break;
