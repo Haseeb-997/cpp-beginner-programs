24947f17904d1cd43492898fcc17e66d34f356c9
Muhammad Haseeb <mhasop997@gmail.com>
2025-08-22 12:16:49 +0500

    Update 1.cpp

--- Exercises/Patterns/Asterisk Patterns/iomanip/1.cpp
+++ Exercises/Patterns/Asterisk Patterns/iomanip/1.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "\n#" << setw(5) << setfill('-') << "*" << setw(5) << "#" << endl;
+    cout << endl;
+    cout << "#" << setw(5) << setfill('-') << "*" << setw(5) << "#" << endl;
     cout << "#" << setw(6) << setfill('-') << "***" << setw(4) << "#" << endl;
     cout << "#" << setw(7) << setfill('-') << "*****" << setw(3) << "#" << endl;
     cout << "#" << setw(8) << setfill('-') << "*******" << setw(2) << "#" << endl;
-    cout << "#" << setw(9) << setfill('-') << "*********" << setw(1) << "#\n" << endl;
+    cout << "#" << setw(9) << setfill('-') << "*********" << setw(1) << "#" << endl;
+    cout << endl;
     return 0;
+
 }
