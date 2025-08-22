1dfe5f87844441fab3953577fdb2823e6360c564
Muhammad Haseeb <mhasop997@gmail.com>
2025-08-22 12:06:29 +0500

    Add files via upload

---
+++ Exercises/Patterns/Asterisk Patterns/iomanip/3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << endl;
+    cout << right << setw(5) << setfill(' ') << "o" << setw(1) << setfill('*') << "o" << endl;    
+    cout << right << setw(4) << setfill(' ') << "o" << setw(3) << setfill('*') << "o" << endl;
+    cout << right << setw(3) << setfill(' ') << "o" << setw(5) << setfill('*') << "o" << endl;
+    cout << right << setw(2) << setfill(' ') << "o" << setw(7) << setfill('*') << "o" << endl;
+    cout << right << setw(1) << setfill(' ') << "o" << setw(9) << setfill('*') << "o" << endl;
+    cout << right << setw(1) << setfill(' ') << "o" << setw(9) << setfill('*') << "o" << endl;
+    cout << right << setw(2) << setfill(' ') << "o" << setw(7) << setfill('*') << "o" << endl;
+    cout << right << setw(3) << setfill(' ') << "o" << setw(5) << setfill('*') << "o" << endl;
+    cout << right << setw(4) << setfill(' ') << "o" << setw(3) << setfill('*') << "o" << endl;
+    cout << right << setw(5) << setfill(' ') << "o" << setw(1) << setfill('*') << "o" << endl;   
+    cout << endl;     
+    return 0;
+}
