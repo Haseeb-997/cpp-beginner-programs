7954e8ef7d105c3ef09256282dd5b7a3c0c2a04f
Muhammad Haseeb <mhasop997@gmail.com>
2025-10-11 06:15:02 +0500

    Update and rename Exercises/Bitwise_Operators/Masking.cpp to Exercises/Bitwise_Operators.cpp

--- Exercises/Bitwise_Operators.cpp
+++ Exercises/Bitwise_Operators.cpp
@@ -10,10 +10,10 @@ int main()
     cin >> num;
     cout << "The bit number you want to change: ";
     cin >> bit_num;
-    cout << "The " << bit_num << " bit of " << num << " is " << ((num >> 7) & 1) << endl;
-    cout << num << " in binary before changing the " << bit_num << " bit is " << bitset<32>(num) << endl;
+    cout << "The " << bit_num << " bit of " << num << " is " << ((num >> bit_num) & 1) << endl;
+    cout << num << " in binary before clearing the " << bit_num << " bit is " << bitset<32>(num) << endl;
     mask = ~(1 << bit_num);
     final = num & mask;
-    cout << num << " in binary after changing the " << bit_num << " bit is " << setw(33) << bitset<32>(final) << endl;
+    cout << num << " in binary after clearing the " << bit_num << " bit is " << setw(33) << bitset<32>(final) << endl;
     return 0;
 }
